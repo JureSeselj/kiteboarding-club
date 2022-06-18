e3ded138dfd7eeb3d2abb98b8117e8245d7a7e2c
JureSeselj <jure.seselj@gmail.com>
2022-06-18 08:37:01 +0000

    Changed px value to rem in style.css

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ const menuBtn = document.getElementById("menuBtn")
         const sideNav = document.getElementById("sideNav")
         const menu = document.getElementById("menu")
 
-        sideNav.style.right = "-250px";
+        sideNav.style.right = "-25rem";
 
         menuBtn.onclick = function(){
-            if (sideNav.style.right == "-250px"){
+            if (sideNav.style.right == "-25rem"){
                 sideNav.style.right = "0";
             }
             else {
-                sideNav.style.right = "-250px";
+                sideNav.style.right = "-25rem";
             }
         }
