f7f573de36ac4808d1b8967eae374376b731f394
JureSeselj <jure.seselj@gmail.com>
2022-06-20 16:29:49 +0000

    Changed width to vertical nav bar

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ const menuBtn = document.getElementById("menuBtn")
         const sideNav = document.getElementById("sideNav")
         const menu = document.getElementById("menu")
 
-        sideNav.style.right = "-25rem";
+        sideNav.style.right = "-24rem";
 
         menuBtn.onclick = function(){
-            if (sideNav.style.right == "-25rem"){
+            if (sideNav.style.right == "-24rem"){
                 sideNav.style.right = "0";
             }
             else {
-                sideNav.style.right = "-25rem";
+                sideNav.style.right = "-24rem";
             }
         }
